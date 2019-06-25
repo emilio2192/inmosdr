c3fc33144924403cadbba7795bdfac645f203c9c
lerp2192 <12002629@galileo.edu>
2019-06-25 14:57:56 -0600

    aboutUs finish backend

--- src/app/backend/cms/about-us/about-us.component.ts
+++ src/app/backend/cms/about-us/about-us.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AboutUs} from '../../../models/aboutUs';
+import {FirebaseService} from '../../../firebase.service';
 
 @Component({
     selector: 'app-about-us',
@@ -9,16 +10,41 @@ import {AboutUs} from '../../../models/aboutUs';
 export class AboutUsComponent implements OnInit {
 
     expanded = false;
-    newTopin: AboutUs;
+    newTopin: AboutUs = {} as AboutUs;
+    topins: Array<any> = [];
 
-    constructor() {
+    constructor(private firebaseService: FirebaseService) {
     }
 
     ngOnInit() {
+        this.getItems();
     }
 
     expand = () => {
         this.expanded = !this.expanded;
-    };
+    }
 
+    crear = () => {
+        this.firebaseService.getCollection().collection('aboutUs').add(this.newTopin).then(() => window.location.reload());
+    }
+
+    getItems = () => {
+        this.firebaseService.getCollection().collection('aboutUs').snapshotChanges().subscribe(res => {
+            res.map(item => {
+                // tslint:disable-next-line:max-line-length
+                this.firebaseService.getCollection().collection('aboutUs').doc('' + item.payload.doc.id).valueChanges().subscribe(response => {
+                    this.topins.push({id: item.payload.doc.id, data: response});
+                });
+            });
+        });
+    }
+
+    edit = (element) => {
+        // tslint:disable-next-line:max-line-length
+        this.firebaseService.getCollection().collection('aboutUs').doc('' + element.id).set(element.data).then(() => window.location.reload());
+    }
+
+    delete = (id) => {
+        this.firebaseService.getCollection().collection('aboutUs').doc('' + id).delete().then(() => window.location.reload());
+    }
 }
